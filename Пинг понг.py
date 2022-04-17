5682538217af79cb79ddde78b8e6411e2c5871b6
Anton9993 <102893959+Anton9993@users.noreply.github.com>
2022-04-17 10:30:58 +0400

    update1.3
    Добавлен класс Player.

--- Пинг понг.py
+++ Пинг понг.py
@@ -46,3 +46,17 @@ class GameSprite(sprite.Sprite):
   # метод, отрисовывающий героя на окне
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+  # метод "выстрел" (используем место игрока, чтобы создать там пулю)
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
