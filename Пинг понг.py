6dbe6aadc03d4de6129f6453988d15c536cf76bb
Anton9993 <102893959+Anton9993@users.noreply.github.com>
2022-04-17 10:25:52 +0400

    update1.2
    Добавлен класс GameSprite.

--- Пинг понг.py
+++ Пинг понг.py
@@ -1,22 +1,48 @@
 from pygame import *
-win_width = 700
-
-win_height = 500
-display.set_caption("Shooter")
-window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load("background.jpg"), (win_width, win_height))
-
-clock = time.Clock()
-
-game = True
-while game:
-    window.blit(background, (0,0))
-
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-
-
-    display.update()
-    clock.tick()
-
+from random import randint
+# подгружаем отдельно функции для работы со шрифтом
+font.init()
+font1 = font.SysFont("Arial", 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+ 
+ 
+font2 = font.Font(None, 36)
+ 
+#фоновая музыка
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('fire.ogg')
+ 
+# нам нужны такие картинки:
+img_back = "galaxy.jpg" # фон игры
+ 
+img_bullet = "bullet.png" # пуля
+img_hero = "rocket.png" # герой
+img_enemy = "ufo.png" # враг
+ 
+score = 0 # сбито кораблей
+goal = 10 # столько кораблей нужно сбить для победы
+lost = 0 # пропущено кораблей
+max_lost = 3 # проиграли, если пропустили столько
+ 
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+  # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+ 
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+ 
+  # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
