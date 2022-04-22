a831205f9599a9053d8b6dea792a2408ddfc46d2
Anton9993 <102893959+Anton9993@users.noreply.github.com>
2022-04-22 20:04:09 +0400

    V3 update
    добавлено движение.

--- Пинг понг.py
+++ Пинг понг.py
@@ -4,7 +4,7 @@ win_width = 700
 win_height = 500
 display.set_caption("Shooter")
 window = display.set_mode((win_width, win_height))
-background = transform.scale(image.load("background.jpg"), (win_width, win_height))
+background = transform.scale(image.load("fon.jpg"), (win_width, win_height))
 
 clock = time.Clock()
 
@@ -31,10 +31,18 @@ class Player(GameSprite):
     # метод для управления спрайтом стрелками клавиатуры
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.y > 5:
+        if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_RIGHT] and self.rect.y < win_height - 80:
+        if keys[K_DOWN] and self.rect.y < win_height - 130:
             self.rect.y += self.speed
+
+    def update2(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 130:
+            self.rect.y += self.speed
+
   # метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def fire(self):
         bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
@@ -53,25 +61,53 @@ class Enemy(GameSprite):
             lost = lost + 1
 
 
-left_r = Player('ruka1.png', 10, 10, 85, 130,5)
-right_r = Player('ruka2.png', 650, 10, 100, 130,5)
-ball = GameSprite('pelmenball.png', 200,200,50,20,2)
+left_r = Player('rukar.png', 0, 150, 100, 130,5)
+right_r = Player('rukal.png', 600, 165, 100, 130,5)
+ball = GameSprite('ball.png', 300,150,130,130,2)
+
+font.init()
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 
 game = True
 while game:
     window.blit(background, (0,0))
 
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
+
+    if ball.rect.y > win_height-50 or ball.rect.y < 0:
+           speed_y *= -1
+
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    if sprite.collide_rect(left_r, ball)   or sprite.collide_rect(right_r, ball):
+           speed_x *= -1
+
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
     left_r.reset()
     right_r.reset()
     ball.reset()
+    left_r.update()
+    right_r.update2()
+    
 
     display.update()
     clock.tick()
 
+
+
+    
 
 
     
